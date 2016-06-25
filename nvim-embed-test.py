c86f364bc8408f9f78933440c45712194cb58cc8
Lukas Holecek <hluk@email.cz>
2016-06-25 11:28:52 +0200

    Show selections and block cursor

--- nvim-embed-test.py
+++ nvim-embed-test.py
@@ -13,22 +13,34 @@ class NVim ():
         self.nvim = neovim.attach('child', argv=['nvim', '--embed'])
 
     def setText(self, text):
-        self.nvim.buffers[0][:] = text.split('\n')
+        self.buffer()[:] = text.split('\n')
 
     def text(self):
-        return '\n'.join(self.nvim.buffers[0][:])
+        return '\n'.join(self.buffer()[:])
+
+    def buffer(self):
+        return self.nvim.current.buffer
 
     def cursorPosition(self):
         return self.nvim.windows[0].cursor
 
+    def cursorAnchor(self):
+        return [self.nvim.eval('line("v")'), self.nvim.eval('col("v")')]
+
     def keyPress(self, key):
         self.nvim.feedkeys(key, 't')
+
+    def mode(self):
+        return self.eval('mode()')
 
     def commandLine(self):
         return self.eval('getcmdline()')
 
     def commandLineType(self):
         return self.eval('getcmdtype()')
+
+    def byte(self, row, col):
+        return self.eval('line2byte(' + str(row) + ')') + col - 1
 
     def eval(self, expr):
         return self.nvim.eval(expr)
@@ -42,27 +54,81 @@ class Editor (QTextEdit):
         self.nvim = nvim
         self.statusBar = statusBar
 
-        self.setPlainText(self.nvim.text())
+        self.update()
 
     def keyPressEvent(self, e):
         key = e.text()
         if key:
             self.nvim.keyPress(key)
+            self.update()
 
-            self.setPlainText(self.nvim.text())
+    def update(self):
+        self.setPlainText(self.nvim.text())
 
-            row, col = self.nvim.cursorPosition()
-            cursor = QTextCursor(self.document())
-            cursor.movePosition(QTextCursor.NextBlock, QTextCursor.MoveAnchor, row - 1)
-            cursor.movePosition(QTextCursor.NextCharacter, QTextCursor.MoveAnchor, col)
-            self.setTextCursor(cursor)
+        mode = self.nvim.mode()
 
-            self.statusBar.setCommandLineType(self.nvim.commandLineType())
-            self.statusBar.setCommandLine(self.nvim.commandLine())
+        row, col = self.nvim.cursorPosition()
+        cursor = QTextCursor(self.document())
+        cursor.setPosition(self.nvim.byte(row, col))
+        self.setTextCursor(cursor)
+
+        selections = []
+
+        if mode != 'i' and mode != 'v' and mode != 'V' and mode != '' and mode[0] != 'c':
+            selection = QTextEdit.ExtraSelection()
+            selection.cursor = cursor
+            selection.cursor.movePosition(QTextCursor.NextCharacter, QTextCursor.KeepAnchor)
+            selection.format = QTextCharFormat()
+            selection.format.setBackground(Qt.black)
+            selection.format.setForeground(Qt.white)
+            selections.append(selection)
+
+        if mode == 'v' or mode == 'V' or mode == '':
+            row2, col2 = self.nvim.cursorAnchor()
+            if row2 > 0 and col2 >= 0:
+                begin = self.nvim.byte(row, col)
+                end = self.nvim.byte(row2, col2)
+
+                if end <= begin + 1:
+                    begin += 1
+                    end -= 1
+
+                selection = QTextEdit.ExtraSelection()
+
+                selection.cursor = QTextCursor(self.document())
+
+                selection.cursor.setPosition(begin)
+                if mode == 'V':
+                    if begin <= end:
+                        selection.cursor.movePosition(QTextCursor.StartOfBlock)
+                    else:
+                        selection.cursor.movePosition(QTextCursor.EndOfBlock)
+
+                selection.cursor.setPosition(end, QTextCursor.KeepAnchor)
+                if mode == 'V':
+                    if end <= begin:
+                        selection.cursor.movePosition(QTextCursor.StartOfBlock, QTextCursor.KeepAnchor)
+                    else:
+                        selection.cursor.movePosition(QTextCursor.EndOfBlock, QTextCursor.KeepAnchor)
+
+                selection.format = QTextCharFormat()
+                selection.format.setBackground(Qt.black)
+                selection.format.setForeground(Qt.white)
+                selections.append(selection)
+
+        self.setExtraSelections(selections)
+
+
+        self.statusBar.setCommandLineType(self.nvim.commandLineType())
+        self.statusBar.setCommandLine(self.nvim.commandLine())
+        self.statusBar.setMode(mode)
 
 class StatusBar (QStatusBar):
     def __init__(self, window):
         super(QStatusBar, self).__init__(window)
+
+        self.mode = QLabel(self)
+        self.addPermanentWidget(self.mode)
 
         self.commandLineType = QLabel(self)
         self.addPermanentWidget(self.commandLineType)
@@ -79,6 +145,9 @@ class StatusBar (QStatusBar):
 
     def setCommandLine(self, text):
         self.commandLine.setText(text)
+
+    def setMode(self, text):
+        self.mode.setText(text)
 
 class MainWindow (QMainWindow):
     def __init__(self, nvim, parent = None):
